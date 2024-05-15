fc2e4e2bb7261121e09de9abc609aaadec00bde1
druid3122 <asta199301@outlook.com>
2024-05-15 15:36:17 +0800

    modify anker rpc is default RPC

--- src/main/java/com/nulink/livingratio/service/StakeRewardOverviewService.java
+++ src/main/java/com/nulink/livingratio/service/StakeRewardOverviewService.java
@@ -103,7 +103,7 @@ public class StakeRewardOverviewService {
         if (null != rewardOverview){
             try {
                 String pvoStr = JSON.toJSONString(rewardOverview, SerializerFeature.WriteNullStringAsEmpty);
-                redisService.set(stakeRewardOverviewFindEpoch, pvoStr, 29, TimeUnit.MINUTES);
+                redisService.set(stakeRewardOverviewFindEpoch, pvoStr, 15, TimeUnit.MINUTES);
             }catch (Exception e){
                 log.error("StakeRewardOverview findLastEpoch redis write error：{}", e.getMessage());
             }
